96ad5b05475391880809e0002f7ccaa7e659d0b6
Ellen Ntiamoah <ellenntiamoah@gmail.com>
2024-07-09 16:11:59 +0000

    updated index

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { dbconnection } from "./config/db.js";
+import eventsRouter from "./routes/event-model.js";
+import 'dotenv/config'
 
 dbconnection();
 
 const app = express();
 
+app.use(eventsRouter)
 
 const port =  7090;
 app.listen(port, () => {
