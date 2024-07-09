48f2d42918e5ded6a25d3a9a4c5a5c246234f42b
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-07-09 16:04:01 +0000

    college-work

---
+++ models/college-model.js
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+
+const collegeSchema = new Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    banner: {type: String, required: true},
+    location: {type: String, required: true},
+    rating: {type: Number, required: true},
+
+})
+
+export const collegeModel = model('college', collegeSchema);
---
+++ routes/college-routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { getCollege, getACollege,postCollege,deleteCollege,updateCollege } from "../controllers/college-controller";
+
+export const collegeRouter = Router();
+
+collegeRouter.get('/college', getcollege);
+collegeRouter.post('/college', postCollege);
+collegeRouter.get('/college/:id', getACollege);
+collegeRouter.patch('/college/:id', updateCollege);
