727995f4606c06b909a3e7e17bc68744389e664d
giftyamoakowaa <maameamoakowaa1@gmail.com>
2024-07-10 13:42:10 +0000

    router_imorted

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
-// import { collegeRouter } from "./routes/college-routes.js";
+import { collegeRouter } from "./routes/college-routes.js";
 import { dbconnection } from "./config/db.js";
 
 
 dbconnection();
 
 const app = express();
-// app.use(express.json());
-// app.use (collegeRouter);
+app.use(express.json());
+app.use (collegeRouter);
 
 const port =  7090;
 app.listen(port, () => {
